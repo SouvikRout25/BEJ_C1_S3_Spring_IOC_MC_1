01fc5700b2e16af61087940dd128111c3528848c
SOUVIK ROUT <souvikrout.21@gmail.com>
2022-10-31 17:13:45 +0530

    done

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -1,2 +1,19 @@
-package org.example;public class AppConfig {
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+
+public class AppConfig {
+    @Bean("employee")
+    public Employee getEmployeeDetails(){
+        Employee employee = new Employee(getDepartmentDetails());
+        employee.setEmpId(101);
+        employee.setEmpName("L Lawleitte");
+        employee.setSalary(880000.50);
+        return employee;
+    }
+
+    @Bean
+    public Department getDepartmentDetails(){
+        return new Department(145,"Teacher");
+    }
 }
--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,2 +1,61 @@
-package org.example;public class Employee {
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+    private int empId;
+    private String empName;
+    private double salary;
+
+    private Department department;
+
+    public Employee() {
+    }
+
+    @Autowired
+    public Employee(Department department) {
+        this.department = department;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", salary=" + salary +
+                ", department=" + department +
+                '}';
+    }
 }
